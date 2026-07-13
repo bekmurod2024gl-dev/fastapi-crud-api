0a98f594882db7ad6d30a7123cfd7a18d9d25e6e
Bobomurod <bekmurod2024gl@gmail.com>
2026-07-12 22:26:12 -1200

    Fix database.py

--- database.py
+++ database.py
@@ -7,6 +7,21 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-print("DATABASE_URL =", DATABASE_URL)   # <-- shu qatorni qo'shing
+print("DATABASE_URL =", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
